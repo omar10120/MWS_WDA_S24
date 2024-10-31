74b8d61f47d53fa6afb9ac7af5e110ccbfbe35c2
tamer tamer <amr.dawoodi@hotmail.com>
2024-10-31 19:52:29 +0300

    update python file

--- src/Component/Student.jsx
+++ src/Component/Student.jsx
@@ -28,7 +28,7 @@ function Student() {
                     </tr>
                     </thead>
                     <tbody>
-                            {Student.map((item)=>(
+                            {Student.map((item , index)=>(
 
                     <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 ">
                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
